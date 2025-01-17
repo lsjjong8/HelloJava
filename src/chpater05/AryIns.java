d9195e9554f5b9f8c8cecf583d951942a272a452
Lee Seungjong <lsjjong8@naver.com>
2025-01-17 16:20:49 +0900

    ~ 2025-01-17 자바 예제 커밋(chapter 05)

---
+++ src/chpater05/AryIns.java
@@ -0,0 +1,37 @@
+package chpater05;
+
+import java.util.Scanner;
+
+public class AryIns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("요소 수:");
+        int num = sc.nextInt();
+        int[] inputArray = new int[num];
+
+        for (int i = 0; i < inputArray.length; i++) {
+            System.out.print("a[" + i + "] =");
+            inputArray[i] = sc.nextInt();
+        }
+
+        System.out.print("삽입할 요소의 인덱스:");
+        int idx = sc.nextInt();
+
+        System.out.print("삽입할 값:");
+        int value = sc.nextInt();
+        
+        aryIns(inputArray, idx, value); // inputArray[idx]에 value를 삽입
+        
+        PrintArray.printIntArray(inputArray);
+    }
+
+    public static void aryIns(int[] arr, int idx, int value) {
+        if(idx >= 0 && idx < arr.length) {
+            for (int i = arr.length-1; i > idx; i--) {
+                arr[i] = arr[i-1];
+            }
+            arr[idx] = value;
+        }
+    }
+}
